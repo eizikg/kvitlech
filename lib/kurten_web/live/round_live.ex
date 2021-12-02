e78d5b6e833a2565a914dc61f784a86e7d542fdc
Eizik Gottesfeld <egottesfeld@dasmenresidential.com>
2021-12-02 15:35:47 -0500

    show amount for standing

--- lib/kurten_web/live/round_live.ex
+++ lib/kurten_web/live/round_live.ex
@@ -243,11 +243,14 @@ defmodule KurtenWeb.RoundLive do
             <span class="flex text-gray-400	text-center justify-center">ｘ</span>
           <% end %>
         <%= if assigns.turn.state == :standby do %>
-          <span class="flex justify-center pt-2 h-min text-gray-400	text-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
-            </svg>
-          </span>
+            <div class="flex justify-center align-center text-center pt-2 h-full text-gray-400	text-center justify-center">
+            <div class="h-6 flex justify-center align-center items-center text-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
+              </svg>
+            </div>
+            <text class="flex pb-2 items-center text-center text-blue-800"><%= "#{assigns.turn.bet}" %></text>
+          </div>
         <% end %>
         <%= if assigns.turn.state == :won do %>
           <span class="flex text-green-700 text-center justify-center"><%= "#{assigns.turn.bet}" %></span>
